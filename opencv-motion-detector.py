7ad23b6552993f469255a190fe3ef609077a5246
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2016-02-03 18:50:12 +0200

    OpenCV Stabilization Detection mechanism added.

--- opencv-motion-detector.py
+++ opencv-motion-detector.py
@@ -8,6 +8,9 @@ import datetime
 import imutils
 import time
 import cv2
+import numpy
+
+STABILIZATION_DETECTION = 20
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -25,14 +28,18 @@ else:
 	camera = cv2.VideoCapture(args["video"])
 
 # initialize the first frame in the video stream
-firstFrame = None
+referenceFrame = None
 
+frame_counter = 0
+on_delta_situation = 0
+delta_value_stack = []
 # loop over the frames of the video
 while True:
+	frame_counter += 1
 	# grab the current frame and initialize the occupied/unoccupied
 	# text
 	(grabbed, frame) = camera.read()
-	text = "Unoccupied"
+	delta_value = 0
 
 	# if the frame could not be grabbed, then we have reached the end
 	# of the video
@@ -40,47 +47,61 @@ while True:
 		break
 
 	# resize the frame, convert it to grayscale, and blur it
-	frame = imutils.resize(frame, width=500)
+	#frame = imutils.resize(frame, width=500)
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
 	# if the first frame is None, initialize it
-	if firstFrame is None:
-		firstFrame = gray
+	if referenceFrame is None:
+		referenceFrame = gray
 		continue
 
 	# compute the absolute difference between the current frame and
 	# first frame
-	frameDelta = cv2.absdiff(firstFrame, gray)
+	frameDelta = cv2.absdiff(referenceFrame, gray)
 	thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
 
 	# dilate the thresholded image to fill in holes, then find contours
 	# on thresholded image
-	thresh = cv2.dilate(thresh, None, iterations=2)
+	#thresh = cv2.dilate(thresh, None, iterations=2)
 	(cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 		cv2.CHAIN_APPROX_SIMPLE)
 
+	countour_stack = []
 	# loop over the contours
-	for c in cnts:
-		# if the contour is too small, ignore it
-		if cv2.contourArea(c) < args["min_area"]:
-			continue
+	if cnts:
+		for c in cnts:
+			countour_stack.append(cv2.contourArea(c))
+			if cv2.contourArea(c) > args["min_area"]:
+				# compute the bounding box for the contour, draw it on the frame,
+				# and update the text
+				(x, y, w, h) = cv2.boundingRect(c)
+				cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+		delta_value = max(countour_stack)
 
-		# compute the bounding box for the contour, draw it on the frame,
-		# and update the text
-		(x, y, w, h) = cv2.boundingRect(c)
-		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-		text = "Occupied"
+		# if the contour is too small, ignore it
+		if delta_value > args["min_area"]:
+			on_delta_situation = 1
+
+	if on_delta_situation:
+		delta_value_stack.append(delta_value)
+		if len(delta_value_stack) >= STABILIZATION_DETECTION:
+			delta_value_stack.pop(0)
+			if min(delta_value_stack) > (numpy.mean(delta_value_stack) - args["min_area"] / 2) and max(delta_value_stack) < (numpy.mean(delta_value_stack) + args["min_area"] / 2):
+				on_delta_situation = 0
+				delta_value_stack = []
+				referenceFrame = None
 
 	# draw the text and timestamp on the frame
-	cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-	cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-		(10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+	cv2.putText(frame, "Diff    : {}".format(delta_value), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+	cv2.putText(frame, "Thresh : {}".format(args["min_area"]), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+	cv2.putText(frame, "Frame : {}".format(frame_counter), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+	cv2.putText(frame, "Delta  : {}".format(on_delta_situation), (10, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+	cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
 
 	# show the frame and record if the user presses a key
-	cv2.imshow("Security Feed", frame)
-	cv2.imshow("Thresh", thresh)
+	cv2.imshow("Original Frame", frame)
+	cv2.imshow("Frame Threshhold", thresh)
 	cv2.imshow("Frame Delta", frameDelta)
 	key = cv2.waitKey(1) & 0xFF
 
