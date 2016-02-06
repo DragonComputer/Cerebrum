a25d17e2bdf551297d11ee370b5526fc3d8b6247
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2016-02-06 23:39:22 +0200

    Vision: OpenCV Optical Flow optimized.

--- vision/opencv-optical-flow.py
+++ vision/opencv-optical-flow.py
@@ -27,44 +27,57 @@ lk_params = dict( winSize  = (15,15),
                   maxLevel = 2,
                   criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
-# Create some random colors
-color = np.random.randint(0,255,(100,3))
+while True:
+    # Create some random colors
+    color = np.random.randint(0,255,(100,3))
 
-# Take first frame and find corners in it
-ret, old_frame = cap.read()
-old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+    # Take first frame and find corners in it
+    ret, old_frame = cap.read()
+    old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+    #p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+    #print p0
+    #print type(p0[0][0][0])
 
-# Create a mask image for drawing purposes
-mask = np.zeros_like(old_frame)
+    p0 = np.random.randint(720, size=(100, 1, 2))
+    p0 = p0.astype(np.float32)
+    print p0
+    print type(p0[0][0][0])
 
-while(1):
-    ret,frame = cap.read()
-    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Create a mask image for drawing purposes
+    mask = np.zeros_like(old_frame)
 
-    # calculate optical flow
-    p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+    while True:
+        ret,frame = cap.read()
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Select good points
-    good_new = p1[st==1]
-    good_old = p0[st==1]
+        # calculate optical flow
+        p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
 
-   # draw the tracks
-    for i,(new,old) in enumerate(zip(good_new,good_old)):
-        a,b = new.ravel()
-        c,d = old.ravel()
-        cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
-        cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
-    img = cv2.add(frame,mask)
+        if p1 is None:
+            break
 
-    cv2.imshow('frame',img)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
+        # Select good points
+        good_new = p1[st==1]
+        good_old = p0[st==1]
 
-    # Now update the previous frame and previous points
-    old_gray = frame_gray.copy()
-    p0 = good_new.reshape(-1,1,2)
+       # draw the tracks
+        for i,(new,old) in enumerate(zip(good_new,good_old)):
+            a,b = new.ravel()
+            c,d = old.ravel()
+            cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
+            cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
+        img = cv2.add(frame,mask)
+
+        cv2.imshow('frame',img)
+
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
+
+        # Now update the previous frame and previous points
+        old_gray = frame_gray.copy()
+        p0 = good_new.reshape(-1,1,2)
+
 
 cv2.destroyAllWindows()
 cap.release()
