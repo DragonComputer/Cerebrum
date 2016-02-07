6302a715e2f5fc028f70b88d681da1d0233e39cc
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2016-02-07 23:18:14 +0200

    Vision: OpenCV retouch and comment lines added.

--- vision/opencv-motion-detector.py
+++ vision/opencv-motion-detector.py
@@ -1,8 +1,7 @@
 # USAGE
 # python motion_detector.py
-# python motion_detector.py --video videos/example_01.mp4
+# python motion_detector.py --video PATH/example_01.mp4
 
-# import the necessary packages
 import argparse
 import datetime
 import imutils
@@ -13,6 +12,7 @@ import os
 
 STABILIZATION_DETECTION = 5
 
+# Define record files's path and filenames
 if not os.path.isfile("memory/" + str(datetime.date.today()) + ".avi"):
 	AVI_OUTPUT_FILENAME_ORIGINAL = "memory/" + str(datetime.date.today()) + ".avi"
 	AVI_OUTPUT_FILENAME_THRESH = "memory/" + str(datetime.date.today()) + "-thresh.avi"
@@ -26,134 +26,128 @@ else:
 	AVI_OUTPUT_FILENAME_DELTA_COLORED = "memory/." + str(datetime.date.today()) + "-delta-colored-TEMP.avi"
 	MULTIPLE_RECORDS = 1
 
-CODEC = cv2.cv.CV_FOURCC('X','V','I','D')
+CODEC = cv2.cv.CV_FOURCC('X','V','I','D') # Define desired codec (Only XVID tested)
 
+# Create video record files. CAUTION: VideoWriter always removes existing files.
 original_out = cv2.VideoWriter(AVI_OUTPUT_FILENAME_ORIGINAL, CODEC, 25.0, (640,360))
 thresh_out = cv2.VideoWriter(AVI_OUTPUT_FILENAME_THRESH, CODEC, 25.0, (640,360))
 delta_out = cv2.VideoWriter(AVI_OUTPUT_FILENAME_DELTA, CODEC, 25.0, (640,360))
 delta_colored_out = cv2.VideoWriter(AVI_OUTPUT_FILENAME_DELTA_COLORED, CODEC, 25.0, (640,360))
 
-# construct the argument parser and parse the arguments
+# Construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="path to the video file")
 ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
 args = vars(ap.parse_args())
 
-# if the video argument is None, then we are reading from webcam
-if args.get("video", None) is None:
+if args.get("video", None) is None: # If the video argument is None, then we are reading from webcam
 	camera = cv2.VideoCapture(0)
 	time.sleep(0.25)
-
-# otherwise, we are reading from a video file
-else:
+else:								# Otherwise, we are reading from a video file
 	camera = cv2.VideoCapture(args["video"])
 
-# initialize the first frame in the video stream
-referenceFrame = None
+referenceFrame = None # Initialize the first frame in the video stream
 
-frame_counter = 0
-on_delta_situation = 0
+(grabbed, first_frame) = camera.read() # Grab the first frame
+
+height, width = first_frame.shape[:2] # Get video height and width (size)
+#if not height == 720 or not width == 1280:
+
+if float(width) / float(height) != float(16) / float(9):
+	raise ValueError('Aspect ratio of input stream must be [16:9]')
+
+frame_counter = 0 # Define frame counter variable
+on_delta_situation = 0 # Delta situation checking variable
 delta_value_stack = []
-# loop over the frames of the video
-while True:
-	frame_counter += 1
-	# grab the current frame and initialize the occupied/unoccupied
-	# text
-	(grabbed, frame) = camera.read()
 
-	# if the frame could not be grabbed, then we have reached the end
-	# of the video
-	if not grabbed:
+while True: # Loop over the frames of the video
+
+	frame_counter += 1 # Increase frame counter's value
+
+	(grabbed, frame) = camera.read() # Grab the current frame and initialize the occupied/unoccupied
+
+	if not grabbed: # If the frame could not be grabbed, then we have reached the end of the video
 		break
 
-	delta_value = 0
+	delta_value = 0 # Delta Value for storing max continuous contour area for current frame
 
-	height, width = frame.shape[:2]
-	if not height == 720 or not width == 1280:
-		raise ValueError('Aspect ratio of input stream must be [16:9]')
-
+	frame = imutils.resize(frame, height=360) # Resize frame to 360p. Alternative resizing method:
 	#frame = cv2.resize(frame,None,fx=0.5, fy=0.5, interpolation = cv2.INTER_CUBIC)
-	frame = imutils.resize(frame, height=360)
 
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Convert frame to grayscale
 
-	# resize the frame, convert it to grayscale, and blur it
-	#frame = imutils.resize(frame, width=500)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+	gray = cv2.bilateralFilter(gray,9,75,75) # Blur current frame with Bilateral Filter for noise reduction
+	# http://docs.opencv.org/master/d4/d13/tutorial_py_filtering.html
+	# Alternative Blur Methods:
 	#gray = cv2.blur(gray,(12,12))
 	#gray = cv2.GaussianBlur(gray, (5, 5), 0)
-	gray = cv2.bilateralFilter(gray,9,75,75)
 
-	# if the first frame is None, initialize it
-	if referenceFrame is None:
+	if referenceFrame is None: # If Reference Frame is None, initialize it
 		referenceFrame = gray
 		continue
 
-	# compute the absolute difference between the current frame and
-	# first frame
-	frameDelta = cv2.absdiff(referenceFrame, gray)
-	thresh = cv2.threshold(frameDelta, 12, 255, cv2.THRESH_BINARY)[1]
-	#Below line adaptive threshold is not good for this project
+	frameDelta = cv2.absdiff(referenceFrame, gray) # Compute the absolute difference between the current frame and reference frame
+	thresh = cv2.threshold(frameDelta, 12, 255, cv2.THRESH_BINARY)[1] # Apply OpenCV's threshold function to get binary frame
+	# Below line adaptive threshold is not good for this project
 	#thresh = cv2.adaptiveThreshold(frameDelta,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
-	# dilate the thresholded image to fill in holes
-	thresh = cv2.dilate(thresh, None, iterations=1)
-	frameDeltaColored = cv2.bitwise_and(frame,frame, mask= thresh)
 
-	# dilate the thresholded image to fill in holes, then find contours
-	# on thresholded image
+	thresh = cv2.dilate(thresh, None, iterations=1) # Dilate the thresholded image to fill in holes
+	frameDeltaColored = cv2.bitwise_and(frame,frame, mask= thresh) # Bitwise and - to get delta frame
+
+	# Find contours on thresholded image
 	(cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 		cv2.CHAIN_APPROX_SIMPLE)
 
-	countour_stack = []
-	# loop over the contours
+	contour_stack = [] # List of contour areas's values
+
+	# Loop over the contours
 	if cnts:
-		for c in cnts:
-			countour_stack.append(cv2.contourArea(c))
-			if cv2.contourArea(c) > args["min_area"]:
-				# compute the bounding box for the contour, draw it on the frame,
-				# and update the text
-				(x, y, w, h) = cv2.boundingRect(c)
-				cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-		delta_value = max(countour_stack)
+		for c in cnts: # Contour in Contours
+			contour_stack.append(cv2.contourArea(c)) # Calculate contour area and append to contour stack
+			if cv2.contourArea(c) > args["min_area"]: # If contour area greater than min area
+				(x, y, w, h) = cv2.boundingRect(c) # Compute the bounding box for this contour
+				cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2) # Draw it on the frame
+		delta_value = max(contour_stack) # Assign max contour area to delta value
 
-		# if the contour is too small, ignore it
-		if delta_value > args["min_area"]:
-			on_delta_situation = 1
+		if delta_value > args["min_area"]: # If max contour area (delta value) greater than min area
+			on_delta_situation = 1 # Initialize delta situation
 
-	if on_delta_situation:
-		delta_value_stack.append(delta_value)
+	if on_delta_situation: # If we are on delta situation
 
-		original_out.write(frame)
-		thresh_out.write(cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR))
-		delta_out.write(cv2.cvtColor(frameDelta, cv2.COLOR_GRAY2BGR))
-		delta_colored_out.write(frameDeltaColored)
+		delta_value_stack.append(delta_value) # Append max contour area (delta value) to delta value stack
 
-		if len(delta_value_stack) >= STABILIZATION_DETECTION:
-			delta_value_stack.pop(0)
+		original_out.write(frame) # Write Original Frame to file
+		thresh_out.write(cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)) # Write Frame Threshhold to file
+		delta_out.write(cv2.cvtColor(frameDelta, cv2.COLOR_GRAY2BGR)) # Write Frame Delta to file
+		delta_colored_out.write(frameDeltaColored) # Write Frame Delta Colored to file
+
+		if len(delta_value_stack) >= STABILIZATION_DETECTION: # If length of delta value stack is greater than or equal to STABILIZATION_DETECTION constant
+			delta_value_stack.pop(0) # Pop first element of delta value stack
+			# If minimum delta value is greater than (mean of last 5 frame - minimum area / 2) and maximum delta value is less than (mean of last 5 frame + minimum area / 2)
 			if min(delta_value_stack) > (numpy.mean(delta_value_stack) - args["min_area"] / 2) and max(delta_value_stack) < (numpy.mean(delta_value_stack) + args["min_area"] / 2):
-				on_delta_situation = 0
-				delta_value_stack = []
-				referenceFrame = None
+				on_delta_situation = 0 # Then video STABILIZED
+				delta_value_stack = [] # Empty delta value stack
+				referenceFrame = None  # Clear reference frame
 
-				#if MULTIPLE_RECORDS:
-
-	# draw the text and timestamp on the frame
+	# Draw the text and timestamp on the frame
 	cv2.putText(frame, "Diff    : {}".format(delta_value), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
 	cv2.putText(frame, "Thresh : {}".format(args["min_area"]), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
 	cv2.putText(frame, "Frame : {}".format(frame_counter), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
 	cv2.putText(frame, "Delta  : {}".format(on_delta_situation), (10, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
 	cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
 
-	# show the frame and record if the user presses a key
+	# Show the frames and record if the user presses ESC or q
 	cv2.imshow("Original Frame", frame)
 	cv2.imshow("Frame Threshhold", thresh)
 	cv2.imshow("Frame Delta", frameDelta)
 	cv2.imshow("Frame Delta Colored", frameDeltaColored)
 	key = cv2.waitKey(1) & 0xFF
 
-	# if the `q` key is pressed, break from the lop
+	# if the `ESC` or `q` key is pressed, break from the loop
 	if key == ord("q") or key == ord("\x1b"):
-		if MULTIPLE_RECORDS:
+		if MULTIPLE_RECORDS: # If there are multiple records
+
+			# Then concatenate them with ffmpeg
 			ffmpeg_concat_original = "ffmpeg -y -i \"concat:memory/" + str(datetime.date.today()) + ".avi|memory/." + str(datetime.date.today()) + "-TEMP.avi\" -c copy memory/.original-TEMP.avi"
 			ffmpeg_concat_thresh = "ffmpeg -y -i \"concat:memory/" + str(datetime.date.today()) + "-thresh.avi|memory/." + str(datetime.date.today()) + "-thresh-TEMP.avi\" -c copy memory/.thresh-TEMP.avi"
 			ffmpeg_concat_delta = "ffmpeg -y -i \"concat:memory/" + str(datetime.date.today()) + "-delta.avi|memory/." + str(datetime.date.today()) + "-delta-TEMP.avi\" -c copy memory/.delta-TEMP.avi"
@@ -171,9 +165,9 @@ while True:
 			os.system("mv memory/.thresh-TEMP.avi memory/" + str(datetime.date.today()) + "-thresh.avi")
 			os.system("mv memory/.delta-TEMP.avi memory/" + str(datetime.date.today()) + "-delta.avi")
 			os.system("mv memory/.delta-colored-TEMP.avi memory/" + str(datetime.date.today()) + "-delta-colored.avi")
+			# Concatenate finished
 
 		break
 
-# cleanup the camera and close any open windows
-camera.release()
-cv2.destroyAllWindows()
+cv2.destroyAllWindows() # Close any open windows
+camera.release() # Release the capture device
--- vision/opencv-optical-flow.py
+++ vision/opencv-optical-flow.py
@@ -1,4 +1,8 @@
-import numpy as np
+# USAGE
+# python opencv-optical-flow.py
+# python opencv-optical-flow.py --video PATH/example_01.mp4
+
+import numpy
 import cv2
 import argparse
 import time
@@ -6,152 +10,114 @@ import random
 import math
 from collections import Counter
 
-TRACKER_POINTS = 2000
-CRAZY_LINE_DISTANCE =  50
-CRAZY_LINE_LIMIT = 100 * TRACKER_POINTS / 1000
+TRACKER_POINTS = 2000 # How many points will be used to track the optical flow
+CRAZY_LINE_DISTANCE =  50 # Distance value to detect crazy lines
+CRAZY_LINE_LIMIT = 100 * TRACKER_POINTS / 1000 # Amount of crazy lines are indication of different shots
+ABSDIFF_ANGLE = 20 # To determine the inconsistency between tangent values in degrees
+LINE_THICKNESS = 3 # Lines thickness that we will use for mask delta
 
-def PolygonArea(corners):
-    n = len(corners) # of corners
-    area = 0.0
-    for i in range(n):
-        j = (i + 1) % n
-        area += corners[i][0] * corners[j][1]
-        area -= corners[j][0] * corners[i][1]
-    area = abs(area) / 2.0
-    return area
-
+# Construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="path to the video file")
 args = vars(ap.parse_args())
 
-# if the video argument is None, then we are reading from webcam
-if args.get("video", None) is None:
+if args.get("video", None) is None: # If the video argument is None, then we are reading from webcam
 	cap = cv2.VideoCapture(0)
 	time.sleep(0.25)
-
-# otherwise, we are reading from a video file
-else:
+else:                               # Otherwise, we are reading from a video file
 	cap = cv2.VideoCapture(args["video"])
 
-# params for ShiTomasi corner detection
-feature_params = dict( maxCorners = 100,
-                       qualityLevel = 0.3,
-                       minDistance = 7,
-                       blockSize = 7 )
-
-# Parameters for lucas kanade optical flow
+# Parameters for Lucas Kanade Optical Flow
 lk_params = dict( winSize  = (15,15),
                   maxLevel = 2,
                   criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
-frame_counter = 0
+while True: # On this level it gets only one frame
 
-while True:
-    # Create some random colors
-    color = np.random.randint(0,255,(TRACKER_POINTS,3))
+    color = numpy.random.randint(0,255,(TRACKER_POINTS,3)) # Create some random colors
 
-    # Take first frame and find corners in it
-    ret, old_frame = cap.read()
-    frame_counter += 1
-    old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-    #p1 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
-    #print p1
-    #print type(p1[0][0][0])
+    ret, old_frame = cap.read() # Take first frame
+    old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY) # Convert previous frame to grayscale
 
-    #For Polygon Model
-    lines = [[]] * TRACKER_POINTS
+    height, width = old_frame.shape[:2] # Get video height and width (size)
 
-    #For Triangle Model
-    p0 = None
+    # Create random points on frame
+    p1 = numpy.random.randint(width, size=(TRACKER_POINTS, 1, 2))
+    for y in p1: # Get y values one by one
+         if y[0][1] > height: # If there is a y value that greater than max height
+             y[0][1] = numpy.random.random_integers(height) # Random again this time with max height value
+    p1 = p1.astype(numpy.float32) # Change numpy array's data type to float32
 
-    p1 = np.random.randint(1280, size=(TRACKER_POINTS, 1, 2))
-    for y in p1:
-         if y[0][1] > 960:
-             y[0][1] = np.random.random_integers(960)
+    mask = numpy.zeros_like(old_frame) # Create a mask image for drawing purposes (original frame)
+    mask_delta = numpy.zeros_like(old_frame) # Create a mask image for drawing purposes (delta frame)
+    white_color = numpy.array([255,255,255]) # Define color white
 
-    #print type(p1[0][0][0])
-    p1 = p1.astype(np.float32)
-    #print p1
-    #print type(p1[0][0][0])
+    total_crazy_lines = 0 # Crazy line counter
 
-    # Create a mask image for drawing purposes
-    mask = np.zeros_like(old_frame)
-    mask_delta = np.zeros_like(old_frame)
-    white_color = np.array([255,255,255])
+    most_common_angle = None # Most common angle in general optical flow in shot
 
-    total_crazy_lines = 0
+    while True: # Loop over the frames of the video
 
-    most_common_angle = None
+        ret,frame = cap.read() # Take a new frame
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Convert it to grayscale
 
-    while True:
-        ret,frame = cap.read()
-        frame_counter += 1
-        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        p2, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p1, None, **lk_params) # Calculate optical flow (Lucas Kanade Optical Flow function of OpenCV)
 
-        # calculate optical flow
-        p2, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p1, None, **lk_params)
-
-        if p2 is None:
-            break
+        if p2 is None: # If there are not any points that coming from Lucas Kanade Optical Flow function of OpenCV
+            break      # Break the loop to reconstruct the optical flow
 
         # Select good points
         good_points2 = p2[st==1]
         good_points1 = p1[st==1]
 
-        angles_array = []
+        angles_array = [] # Angle list of all newly created tiny lines
 
-       # draw the tracks
-        for i,(good_point2,good_point1) in enumerate(zip(good_points2,good_points1)):
-            x2,y2 = good_point2.ravel()
-            x1,y1 = good_point1.ravel()
+        for i,(good_point2,good_point1) in enumerate(zip(good_points2,good_points1)): # Get point pairs one by one
 
-            tup2 = (x2,y2)
-            tup1 = (x1,y1)
+            # Get coordinates of points
+            x2,y2 = good_point2.ravel() # New point
+            x1,y1 = good_point1.ravel() # Previous point
 
-            distance = math.hypot(x2 - x1, y2 - y1)
-            if distance >= CRAZY_LINE_DISTANCE:
-                total_crazy_lines += 1
+            distance = math.hypot(x2 - x1, y2 - y1) # Length of the tiny line between these two points.
 
-            angle = math.atan2(y2-y1, x2-x1)
-            angle = math.degrees(angle)
-            angle = round(angle)
-            #print angle
-            angles_array.append(angle)
+            if distance >= CRAZY_LINE_DISTANCE: # If the line is not that "tiny" it's CRAZY! xD
+                total_crazy_lines += 1          # Then increase total crazy line counter's value
 
-            if most_common_angle != None:
-                if abs(most_common_angle - angle) > 20:
-                    cv2.line(mask_delta, (x2,y2),(x1,y1), white_color, 3)
+            angle = math.atan2(y2-y1, x2-x1) # Calculate tangent value of the line (returns Radian)
+            angle = math.degrees(angle)      # Radian to Degree
+            angle = round(angle)             # Round up the degree
+            angles_array.append(angle)       # Append the degree to Angle List
 
-            cv2.line(mask, (x2,y2),(x1,y1), color[i].tolist(), 1)
-            #cv2.circle(frame,(x2,y2),5,color[i].tolist(),-1)
+            if most_common_angle != None: # If there is a most common angle value
+                if abs(most_common_angle - angle) > ABSDIFF_ANGLE: # If Absolute Difference between most common angle and the current line's angle greater than ABDSDIFF_ANGLE value, this line is inconsistent to current general optical flow
+                    cv2.line(mask_delta, (x2,y2),(x1,y1), white_color, LINE_THICKNESS) # Then draw a white line to mask_delta
 
-        if angles_array:
-            print Counter(angles_array).most_common()[0]
-            most_common_angle = Counter(angles_array).most_common()[0][0]
+            cv2.line(mask, (x2,y2),(x1,y1), color[i].tolist(), 1) # In every frame draw the colored lines to original frame for better understanding (optional)
 
-        img = cv2.add(frame,mask)
+        if angles_array: # If angles_array is not empty
+            most_common_angle = Counter(angles_array).most_common()[0][0] # Find most common angle value in Angle List
 
-        mask_delta_gray = cv2.cvtColor(mask_delta, cv2.COLOR_BGR2GRAY)
-        thresh = cv2.threshold(mask_delta_gray, 12, 255, cv2.THRESH_BINARY)[1]
-        frameDelta = cv2.bitwise_and(frame,frame, mask= thresh)
+        img = cv2.add(frame,mask) # Add mask layer over frame
 
-        cv2.imshow('frame',img)
-        cv2.imshow('mask delta',mask_delta)
-        cv2.imshow('frame delta',frameDelta)
+        mask_delta_gray = cv2.cvtColor(mask_delta, cv2.COLOR_BGR2GRAY) # Conver mask_delta to grayscale
+        thresh = cv2.threshold(mask_delta_gray, 12, 255, cv2.THRESH_BINARY)[1] # Apply OpenCV's threshold function to get binary frame
+        thresh = cv2.dilate(thresh, None, iterations=1) # Dlation to increase white region for surrounding pixels
+        frameDelta = cv2.bitwise_and(frame,frame, mask= thresh) # Bitwise and - to get delta frame
 
-        #print total_crazy_lines
-        if total_crazy_lines >= CRAZY_LINE_LIMIT:
-            break
+        cv2.imshow('Original Frame',img) # Show Original Frame
+        cv2.imshow('Mask Delta',mask_delta) # Show Mask Delta Frame
+        cv2.imshow('Frame Delta',frameDelta) # Show Frame Delta
 
-        k = cv2.waitKey(30) & 0xff
-        if k == 27:
-            break
+        if total_crazy_lines >= CRAZY_LINE_LIMIT: # If amout of total crazy lines is greater than CRAZY_LINE_LIMIT
+            break # Break the loop to reconstruct the optical flow
 
-        # Now update the previous frame and previous points
-        old_gray = frame_gray.copy()
-        #p0 = p1
-        p1 = good_points2.reshape(-1,1,2)
+        k = cv2.waitKey(30) & 0xff # DEVELOPMENT
+        if k == 27: # DEVELOPMENT
+            break # DEVELOPMENT
+
+        old_gray = frame_gray.copy() # Update the previous frame
+        p1 = good_points2.reshape(-1,1,2) # Update previous points
 
 
-cv2.destroyAllWindows()
-cap.release()
+cv2.destroyAllWindows() # Close any open windows
+cap.release() # Release the capture device
