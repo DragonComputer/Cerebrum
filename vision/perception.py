8a23f32fdedf6b8f3b232263b61bb94adda00cf8
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2016-02-10 15:04:33 +0200

    Vision: Perception basic motion detection (STATIONARY CAMERA) capabilities added.

--- vision/perception.py
+++ vision/perception.py
@@ -36,3 +36,81 @@ if float(width) / float(height) != float(16) / float(9):
 frame_counter = 1 # Define frame counter variable
 on_delta_situation = 0 # Delta situation checking variable
 delta_value_stack = [] # List of delta values
+
+while True: # Loop over the frames of the video
+
+	frame_counter += 1 # Increase frame counter's value
+
+	(grabbed, frame) = camera.read() # Grab the current frame and initialize the occupied/unoccupied
+
+	if not grabbed: # If the frame could not be grabbed, then we have reached the end of the video
+		break
+
+	delta_value = 0 # Delta Value for storing max continuous contour area for current frame
+
+	frame = imutils.resize(frame, height=360) # Resize frame to 360p. Alternative resizing method:
+
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Convert frame to grayscale
+
+	gray = cv2.bilateralFilter(gray,9,75,75) # Blur current frame with Bilateral Filter for noise reduction
+
+	if referenceFrame is None: # If Reference Frame is None, initialize it
+		referenceFrame = gray
+		continue
+
+	frameDelta = cv2.absdiff(referenceFrame, gray) # Compute the absolute difference between the current frame and reference frame
+	thresh = cv2.threshold(frameDelta, 12, 255, cv2.THRESH_BINARY)[1] # Apply OpenCV's threshold function to get binary frame
+
+	thresh = cv2.dilate(thresh, None, iterations=1) # Dilate the thresholded image to fill in holes
+	frameDeltaColored = cv2.bitwise_and(frame,frame, mask= thresh) # Bitwise and - to get delta frame
+
+	# Find contours on thresholded image
+	(cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)
+
+	contour_area_stack = [] # List of contour areas's values
+
+	# Loop over the contours
+	if cnts:
+		for c in cnts: # Contour in Contours
+			contour_area_stack.append(cv2.contourArea(c)) # Calculate contour area and append to contour stack
+			if cv2.contourArea(c) > args["min_area"]: # If contour area greater than min area
+				(x, y, w, h) = cv2.boundingRect(c) # Compute the bounding box for this contour
+				cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2) # Draw it on the frame
+		delta_value = max(contour_area_stack) # Assign max contour area to delta value
+
+		if delta_value > args["min_area"]: # If max contour area (delta value) greater than min area
+			on_delta_situation = 1 # Initialize delta situation
+
+	if on_delta_situation: # If we are on delta situation
+
+		delta_value_stack.append(delta_value) # Append max contour area (delta value) to delta value stack
+
+		if len(delta_value_stack) >= STABILIZATION_DETECTION: # If length of delta value stack is greater than or equal to STABILIZATION_DETECTION constant
+			delta_value_stack.pop(0) # Pop first element of delta value stack
+			# If minimum delta value is greater than (mean of last 5 frame - minimum area / 2) and maximum delta value is less than (mean of last 5 frame + minimum area / 2)
+			if min(delta_value_stack) > (numpy.mean(delta_value_stack) - args["min_area"] / 2) and max(delta_value_stack) < (numpy.mean(delta_value_stack) + args["min_area"] / 2):
+				on_delta_situation = 0 # Then video STABILIZED
+				delta_value_stack = [] # Empty delta value stack
+				referenceFrame = None  # Clear reference frame
+
+	# Draw the text and timestamp on the frame
+	cv2.putText(frame, "Diff    : {}".format(delta_value), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+	cv2.putText(frame, "Thresh : {}".format(args["min_area"]), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+	cv2.putText(frame, "Frame : {}".format(frame_counter), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+	cv2.putText(frame, "Delta  : {}".format(on_delta_situation), (10, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+	cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+
+	# Show the frames and record if the user presses ESC or q
+	cv2.imshow("Original Frame", frame)
+	cv2.imshow("Frame Threshhold", thresh)
+	cv2.imshow("Frame Delta", frameDelta)
+	cv2.imshow("Frame Delta Colored", frameDeltaColored)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the `ESC` or `q` key is pressed, break the loop
+	if key == ord("q") or key == ord("\x1b"):
+		break
+
+cv2.destroyAllWindows() # Close any open windows
+camera.release() # Release the capture device
