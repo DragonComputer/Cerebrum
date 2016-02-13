1bf65e12bddf4744957e6f02620020b0a5f99b57
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2016-02-13 04:19:07 +0200

    Hearing: Perception draw_waveform() now drawing all_frames and thresh_frames together.

--- hearing/perception.py
+++ hearing/perception.py
@@ -19,7 +19,8 @@ CHANNELS = 2
 RATE = 44100
 SAVE_PERIOD = 10
 THRESHOLD = 1000
-SILENCE_DETECTION = 5
+SILENCE_DETECTION = 3
+EMPTY_CHUNK = chr(int('000000', 2)) * CHUNK * 4
 WAVE_OUTPUT_FILENAME = "hearing/memory/" +  str(datetime.date.today()) + ".wav"
 
 def save_file():
@@ -55,25 +56,31 @@ def draw_spectrogram():
 
 def draw_waveform():
 	global all_frames
+	global thresh_frames
 	fig = plt.figure(figsize=(20,2))
 	while True:
-		data = ''.join(all_frames[-20:])
+		data = ''.join(all_frames[-50:])
 		data = numpy.fromstring(data, 'int16')
+		data2 = ''.join(thresh_frames[-50:])
+		data2 = numpy.fromstring(data2, 'int16')
 		plt.clf()
 		plt.plot(data, color='silver', alpha=0.7, linestyle='dotted', drawstyle='steps-pre', antialiased="False", linewidth="0.5", rasterized="True")
+		plt.plot(data2, color='#33cc33', alpha=0.7, linestyle='dotted', drawstyle='steps-pre', antialiased="False", linewidth="0.5", rasterized="True")
 		ax = plt.gca()
 		ax.axes.get_xaxis().set_visible(False)
 		ax.axes.get_yaxis().set_visible(False)
-		ax.patch.set_facecolor('black')
-		ax.patch.set_alpha(0.7)
+		ax.patch.set_facecolor('#4d4d4d')
+		#ax.patch.set_alpha(1.0)
 		plt.ylim([-10000,10000])
 		#plt.draw()
 		plt.tight_layout()
 		fig.canvas.draw()
 		img = numpy.fromstring(fig.canvas.tostring_rgb(), dtype=numpy.uint8, sep='')
 		img = img.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+		img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 		img = imutils.resize(img, height=160) # Resize frame to 160p.
 		crop_img = img[15:145, 50:1350]
+		cv2.putText(img, "Seconds : {}".format(int(len(all_frames)/43.06640625)), (70, 35), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 155), 2)
 		cv2.imshow("Waveform",crop_img)
 		cv2.moveWindow("Waveform",300,850)
 		cv2.waitKey(1)
@@ -98,10 +105,12 @@ print("PROCESSING STARTED")
 
 frames = []
 all_frames = []
+thresh_frames = []
 
 #save_counter = 0
 data = wf.readframes(CHUNK)
 all_frames.append(data)
+thresh_frames.append(EMPTY_CHUNK)
 
 thread = threading.Thread(target=draw_waveform)
 thread.daemon = True
@@ -113,11 +122,17 @@ while data != '':
 	#draw_spectrogram(all_frames)
 	data = wf.readframes(CHUNK)
 	all_frames.append(data)
+	thresh_frames.append(EMPTY_CHUNK)
+
 	rms = audioop.rms(data, 2)
 	#print rms
 	if rms >= THRESHOLD:
+		thresh_frames.pop()
+		thresh_frames.pop()
 		frames.append(previous_data)
+		thresh_frames.append(previous_data)
 		frames.append(data)
+		thresh_frames.append(data)
 		silence_counter = 0
 		while silence_counter < SILENCE_DETECTION:
 			stream.write(data)
@@ -125,6 +140,7 @@ while data != '':
 			data = wf.readframes(CHUNK)
 			all_frames.append(data)
 			frames.append(data)
+			thresh_frames.append(data)
 			rms = audioop.rms(data, 2)
 			#print rms
 			if rms < THRESHOLD:
