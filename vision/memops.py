27a780773ad368aeeeeec9de5c4363aa3f81a96b
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2016-02-25 02:21:58 +0200

    Vision | Memops: Timestamp(TSTP) file Read & Write functions added.

--- vision/memops.py
+++ vision/memops.py
@@ -9,6 +9,12 @@ class Memory(object):
 		self.thresh_binary = thresh_binary # Thresh binary frame attribute
 		self.frame_delta_colored = frame_delta_colored # Frame delta colored frame attribute
 
+# Timestamp class
+class Timestamp(object):
+	def __init__(self, starting_time, ending_time): # Initialize the object
+		self.starting_time = starting_time # Starting time attribute
+		self.ending_time = ending_time # Ending time attribute
+
 # Convert object to dictionary
 def makeit_dict(obj):
 	if isinstance(obj, set):
@@ -18,11 +24,17 @@ def makeit_dict(obj):
 # Write a memory function
 def write_memory(thresh_binary, frame_delta_colored, starting_time, ending_time):
 	MEM_FILE_PATH = "vision/memory/" +  str(datetime.date.today()) + ".mem" # Path for mem file
+	TSTP_FILE_PATH = "vision/memory/" +  str(datetime.date.today()) + ".tstp" # Path for tstp file
 
 	memory = Memory(starting_time.strftime("%Y-%m-%d %H:%M:%S:%f"), ending_time.strftime("%Y-%m-%d %H:%M:%S:%f"), thresh_binary, frame_delta_colored) # Create an object from Memory class
 	mode = 'a' if os.path.exists(MEM_FILE_PATH) else 'w' # If memory file exist file open mode is append(a) else write(w)
 	with open(MEM_FILE_PATH, mode) as mem_file: # Open file
 		mem_file.write(str(makeit_dict(memory)) + '\n') # Write memory in only one line
+
+	timestamp = Timestamp(starting_time.strftime("%Y-%m-%d %H:%M:%S:%f"), ending_time.strftime("%Y-%m-%d %H:%M:%S:%f")) # Create an object from Timestamp class
+	mode = 'a' if os.path.exists(TSTP_FILE_PATH) else 'w' # If timestamp file exist file open mode is append(a) else write(w)
+	with open(TSTP_FILE_PATH, mode) as tstp_file: # Open file
+		tstp_file.write(str(makeit_dict(timestamp)) + '\n') # Write timestamp in only one line
 
 # Read a memory function
 def read_memory(date_day,nth_record):
@@ -34,6 +46,17 @@ def read_memory(date_day,nth_record):
 			return memory # Return memory to call
 	else: # If memory file doesn't exist
 		raise ValueError('MEM file doesn\'t exist!') # Raise a ValueError
+
+# Read a timestamp function
+def read_timestamp(date_day,nth_record):
+	TSTP_FILE_PATH = "vision/memory/" +  date_day + ".mem" # Path for tstp file
+
+	if os.path.exists(TSTP_FILE_PATH): # If timestamp file exist
+		with open(TSTP_FILE_PATH, 'r') as tstp_file: # Open file
+			timestamp = eval(tstp_file.readlines()[nth_record]) # Evaluate the line, which will return a dictionary
+			return timestamp # Return timestamp to call
+	else: # If timestamp file doesn't exist
+		raise ValueError('TSTP file doesn\'t exist!') # Raise a ValueError
 
 # Example USAGE block. NOT FUNCTIONAL
 if __name__ == "__main__":
