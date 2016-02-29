a523e95e8e17b38cf056e472a8881478d9282e5d
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2016-03-01 00:01:39 +0200

    README.srt and setup.py updated.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
 	# Versions should comply with PEP440.  For a discussion on single-sourcing
 	# the version across setup.py and the project code, see
 	# https://packaging.python.org/en/latest/single_source_version.html
-	version='0.1.21',
+	version='0.1.22',
 
 	description='A library for Crossmodal Supervised Learning Algorithm with Time Series Memory Recording & Long Short-Term Memory Networks',
 	long_description=long_description,
@@ -32,7 +32,7 @@ setup(
 	url='https://github.com/mertyildiran/Cerebrum',
 
 	# Author details
-	author='Mehmet Mert Yıldıran',
+	author='Mehmet Mert Yildiran',
 	author_email='mert.yildiran@bil.omu.edu.tr',
 
 	# Choose your license
