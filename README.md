773274678c3e253e7a40f47c9a4800c3f13942f2
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2016-03-01 23:34:16 +0200

    NeuralNet | Trainer: Now able to get memories from hearing and vision.

--- README.md
+++ README.md
@@ -45,7 +45,7 @@ Parts of The Cerebrum:
 > pre-trained Artificial Neural Networks.
 
 ### Version
-0.1.28
+0.1.29
 
 ### Installation
 
--- cerebrum/neuralnet/trainer.py
+++ cerebrum/neuralnet/trainer.py
@@ -6,15 +6,41 @@ import crossmodal.cmops # BUILT-IN Crosmodal operations package
 import hearing.memops # BUILT-IN Hearing Memory operations package
 import vision.memops # BUILT-IN Vision Memory operations package
 import time # Provides various time-related functions.
+import pyaudio
+import cv2 # (Open Source Computer Vision) is a library of programming functions mainly aimed at real-time computer vision.
+import numpy # The fundamental package for scientific computing with Python.
 
+CHUNK = 1024 # Smallest unit of audio. 1024 bytes
+FORMAT = pyaudio.paInt16 # Data format
+CHANNELS = 2 # Number of channels
+RATE = 44100 # Bit Rate of audio stream / Frame Rate
 
 # MAIN CODE BLOCK
 def start():
 	pairs = crossmodal.cmops.get_pairs(str(datetime.date.today()), 0) # Get pairs starting from 0th line
 	if not pairs:
-		print "No pairs? BUG?"
+		print "No pairs found."
+		sys.exit()
+
+	p = pyaudio.PyAudio() # Create a PyAudio session
+
+	# Create a stream
+	stream = p.open(format=FORMAT,
+					channels=CHANNELS,
+					rate=RATE,
+					output=True)
 
 	# Loop over the pairs coming from CROSSMODAL
 	for pair in pairs:
 		   time.sleep(0.5) # Wait 0.5 seconds to prevent aggressive loop
-		   print pair
+		   if pair['direction'] == "hearing to vision":
+				hearing_memory = hearing.memops.read_memory(str(datetime.date.today()),pair['timestamp1'])
+				for chunky in hearing_memory['data']:
+					stream.write(chunky)
+				vision_memory = vision.memops.read_memory(str(datetime.date.today()),pair['timestamp2'])
+				print len(vision_memory['thresh_binary'])
+				for frame in vision_memory['thresh_binary']:
+					frame = numpy.fromstring(frame, 'int16').reshape(640,360)
+					print frame.shape
+					#cv2.imshow("Frame Threshhold", frame)
+					#cv2.moveWindow("Frame Threshhold",50,550)
