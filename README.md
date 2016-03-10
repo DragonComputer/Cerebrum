f4459625bcdb2100e1f7c1701b2da947765db499
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2016-03-10 16:29:34 +0200

    Language | CMU Pronouncing Dictionaries added.

--- README.md
+++ README.md
@@ -45,7 +45,7 @@ Parts of The Cerebrum:
 > pre-trained Artificial Neural Networks.
 
 ### Version
-0.1.48
+0.1.49
 
 ### Installation
 
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
 	# Versions should comply with PEP440.  For a discussion on single-sourcing
 	# the version across setup.py and the project code, see
 	# https://packaging.python.org/en/latest/single_source_version.html
-	version='0.1.48',
+	version='0.1.49',
 
 	description='Crossmodal Supervised Learning Toolkit with Time Series Memory Recording & Long Short-Term Memory Networks',
 	long_description=long_description,
@@ -84,7 +84,10 @@ setup(
 	# If there are data files included in your packages that need to be
 	# installed, specify them here.  If using Python 2.6 or less, then these
 	# have to be included in MANIFEST.in as well.
-	package_data={},
+	package_data={
+		# If any package contains *.txt files, include them:
+		'': ['*.txt'],
+	},
 
 	# Although 'package_data' is the preferred approach, in some case you may
 	# need to place data files outside of your packages. See:
